20626eb60eecc0f733b59f2d5534cff8daf1be61
HoMeR <faubertin@financeactive.com>
2017-03-29 15:27:01 +0200

    Prevent issue with old Excel Version with duplicated pictures

--- core/modules/core/src/com/haulmont/yarg/formatters/impl/XLSFormatter.java
+++ core/modules/core/src/com/haulmont/yarg/formatters/impl/XLSFormatter.java
@@ -167,7 +167,7 @@ public class XLSFormatter extends AbstractFormatter {
             cleanupCells(resultSheet);
         }
 
-        copyPicturesToResultWorkbook();
+        //copyPicturesToResultWorkbook();
 
         initNamedStyleCache();
     }
